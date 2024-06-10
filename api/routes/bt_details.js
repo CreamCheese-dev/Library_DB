c801849ffafe8f0e7221ee379ff294c1cce22931
CreamCheese-dev <97072286+CreamCheese-dev@users.noreply.github.com>
2024-06-10 00:42:33 -0400

    Delete api directory
    wrong dir

--- api/routes/bt_details.js
+++
@@ -1,9 +0,0 @@
-// Modified based on Express documentation starter code
-// Source: https://expressjs.com/en/4x/api.html#router
-
-const express = require('express');
-const router = express.Router();
-const bt_detailsController = require('../controllers/bt_detailsController');
-
-router.use('/', bt_detailsController.getAllTransactionDetails);
-module.exports = router;
